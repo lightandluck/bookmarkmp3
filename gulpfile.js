c9ed26eefa9e582d4936fea2e2b7b1981f75f15e
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-09-29 15:59:06 -0700

    Added gulp file for build process and live reload server

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp')
+    connect = require('gulp-connect')
+
+gulp.task('copy-html', _ => {
+    return gulp.src('./src/index.html')
+        .pipe(gulp.dest('./public/'))
+        .pipe(connect.reload())
+})
+
+gulp.task('webserver', _ => {
+    connect.server({
+        root: 'public',
+        livereload: true
+    })
+})
+
+gulp.task('watch', _ => {
+    gulp.watch('./src/index.html', ['copy-html'])
+})
+
+gulp.task('default', ['copy-html', 'webserver', 'watch'])
