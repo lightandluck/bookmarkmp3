b28f5eaf518a3b37f1ed0a6726da46d220ef373a
Kevin Nguyen <kevin.nguyen@lineophile.com>
2016-10-12 12:06:13 -0700

    Clean up: added css file, moved js out of html file, updated gulpfile to copy files

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,38 @@
-var gulp = require('gulp')
-    connect = require('gulp-connect')
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
 
-gulp.task('copy-html', _ => {
+gulp.task('html', _ => {
     return gulp.src('./src/index.html')
         .pipe(gulp.dest('./public/'))
-        .pipe(connect.reload())
-})
+        .pipe(connect.reload());
+});
+
+gulp.task('js', _ => {
+    return gulp.src('./src/*.js')
+        .pipe(gulp.dest('./public/js/'))
+        .pipe(connect.reload());
+});
+
+gulp.task('css', _ => {
+    return gulp.src('./src/*.css')
+        .pipe(gulp.dest('./public/css/'))
+        .pipe(connect.reload());
+});
+
+
 
 gulp.task('webserver', _ => {
     connect.server({
         root: 'public',
         livereload: true,
         port: 9002
-    })
-})
+    });
+});
 
 gulp.task('watch', _ => {
-    gulp.watch('./src/index.html', ['copy-html'])
+    gulp.watch('./src/index.html', ['html']);
+    gulp.watch('./src/*.js', ['js']);
+    gulp.watch('./src/*.css', ['css']);
 })
 
-gulp.task('default', ['copy-html', 'webserver', 'watch'])
+gulp.task('default', ['html', 'js', 'css', 'webserver', 'watch']);
